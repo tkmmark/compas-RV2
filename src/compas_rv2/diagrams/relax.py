5f59e3330aa9ba1c00dbd4ec3ed6a0d6483bee7d
Tom Van Mele <vanmelet@ethz.ch>
2019-12-04 13:09:11 +0100

    stub for diagram relaxation

---
+++ src/compas_rv2/diagrams/relax.py
@@ -0,0 +1,45 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+from compas.numerical import fd_numpy
+
+
+__all__ = [
+    'relax_boundary_openings',
+    'relax_boundary_openings_proxy',
+]
+
+
+def relax_boundary_openings_proxy(formdata, fixed):
+    from compas_rv2.diagrams import FormDiagram
+    form = FormDiagram.from_data(formdata)
+    relax_boundary_openings(form, fixed)
+    return form.to_data()
+
+
+def relax_boundary_openings(form, fixed):
+    """"""
+    k_i   = form.key_index()
+    xyz   = form.get_vertices_attributes(('x', 'y', 'z'))
+    edges = [(k_i[u], k_i[v]) for u, v in form.edges()]
+    fixed = [k_i[key] for key in fixed]
+    q     = form.get_edges_attribute('q')
+    loads = form.get_vertices_attributes(('px', 'py', 'pz'), (0.0, 0.0, 0.0))
+
+    xyz, q, f, l, r = fd_numpy(xyz, edges, fixed, q, loads)
+
+    for key, attr in form.vertices(True):
+        index = k_i[key]
+        attr['x'] = xyz[index][0]
+        attr['y'] = xyz[index][1]
+        attr['z'] = xyz[index][2]
+
+
+# ==============================================================================
+# Main
+# ==============================================================================
+
+if __name__ == '__main__':
+    pass
