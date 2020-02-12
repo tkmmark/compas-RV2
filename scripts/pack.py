c5766c1bfed204155272cfb293d77fe60b31e25e
Li Chen <leochen1990@gmail.com>
2020-02-12 16:36:46 +0100

    add arguments

--- scripts/pack.py
+++ scripts/pack.py
@@ -3,16 +3,35 @@ import os
 import shutil
 import time
 
+
+import argparse
+
+parser = argparse.ArgumentParser(
+    description='RhinoVault2 release package tool.')
+
+parser.add_argument('--skip_packing',action='store_false', help="The path to the plugin directory.")
+parser.add_argument('--format',default='zip', help="The path to the plugin directory.")
+
+args = parser.parse_args()
+
+
 start = time.time()
 conda_pack.pack(output="env.zip", verbose=True, n_threads=-1, force=True)
 
 print('unpacking to ui/Rhino/RV2/dev/env')
 shutil.unpack_archive("env.zip", "ui/Rhino/RV2/dev/env")
-os.remove("env.zip")
 
-print('re-packing to rhi')
-shutil.make_archive( "RV2", "zip", "ui/Rhino/RV2/dev")
-shutil.rmtree("ui/Rhino/RV2/dev/env")
-os.rename("RV2.zip","RV2.rhi")
+if args.skip_packing:
 
-print('finished, took %s s'%(time.time()-start))
+    print('finished, took %s s'%(time.time()-start))
+    print('packing skipped, go to ui/Rhino/RV2/dev and run install.bat')
+
+else:
+
+    os.remove("env.zip")
+    print('re-packing whole plugin')
+    shutil.make_archive( "RV2", "zip", "ui/Rhino/RV2/dev")
+    shutil.rmtree("ui/Rhino/RV2/dev/env")
+    os.rename("RV2.zip","RV2.%s"%args.format)
+
+    print('finished, took %s s'%(time.time()-start)) 
--- src/compas_rv2/install.py
+++ src/compas_rv2/install.py
@@ -1,7 +1,21 @@
 if __name__ == '__main__':
-    print('installing compas_rv2')
+
     from compas_rhino.install import install
-    import compas_rv2
+    from compas_rhino.install_plugin import install_plugin
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description='RhinoVault2 Installation command-line utility.')
+
+    parser.add_argument('--plugin_path', help="The path to the plugin directory.")
+    args = parser.parse_args()
+
+    if args.plugin_path:
+        install_plugin(args.plugin_path, version="6.0")
+
+
+    print('installing compas_rv2')
+    
 
     packages = ['compas','compas_rhino','compas_tna','compas_ags','compas_pattern','compas_rv2']
     install(packages=packages)
