d32b225d75b8d96060cf033be4e1b0b28e0deb66
Li Chen <leochen1990@gmail.com>
2020-02-12 14:49:17 +0100

    use zip for windows

--- scripts/pack.py
+++ scripts/pack.py
@@ -4,15 +4,15 @@ import shutil
 import time
 
 start = time.time()
-conda_pack.pack(output="env.tar.gz", verbose=True, n_threads=-1, force=True)
+conda_pack.pack(output="env.zip", verbose=True, n_threads=-1, force=True)
 
-print('unpacking to ui/Rhino/RV2/env')
-shutil.unpack_archive("env.tar.gz", "ui/Rhino/RV2/env")
-os.remove("env.tar.gz")
+print('unpacking to ui/Rhino/RV2/dev/env')
+shutil.unpack_archive("env.zip", "ui/Rhino/RV2/dev/env")
+os.remove("env.zip")
 
 print('re-packing to rhi')
-shutil.make_archive( "RV2", "zip", "ui/Rhino/")
-shutil.rmtree("ui/Rhino/RV2/env")
+shutil.make_archive( "RV2", "zip", "ui/Rhino/RV2/dev")
+shutil.rmtree("ui/Rhino/RV2/dev/env")
 os.rename("RV2.zip","RV2.rhi")
 
 print('finished, took %s s'%(time.time()-start))
