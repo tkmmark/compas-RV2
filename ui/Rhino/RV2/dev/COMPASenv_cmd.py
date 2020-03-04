9e9b03454bdd3c3908ae0f9c925b46aeba657975
Li <leochen1990@gmail.com>
2020-03-04 16:01:42 +0100

    get conda path from bootstraper

--- ui/Rhino/RV2/dev/COMPASenv_cmd.py
+++ ui/Rhino/RV2/dev/COMPASenv_cmd.py
@@ -5,47 +5,55 @@ import subprocess
 import json
 import os
 
+try:
+    from compas_bootstrapper import CONDA_EXE
+except ImportError:
+    CONDA_EXE = 'conda'
+
+
 def run(cmd):
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    stdout,stderr = p.communicate()
-    return stdout,stderr,p.returncode
+    stdout, stderr = p.communicate()
+    return stdout, stderr, p.returncode
+
 
 def install_env_packages(env_path, packages):
-    python_path = os.path.join(env_path,'python')
+    python_path = os.path.join(env_path, 'python')
     if not os.path.isfile(python_path):
-        python_path = os.path.join(env_path,'bin','python')
+        python_path = os.path.join(env_path, 'bin', 'python')
 
     packages = ' '.join(packages)
-    out, err, code = run('%s -m compas_rhino.install -p %s'%(python_path,packages))
+    out, err, code = run('%s -m compas_rhino.install -p %s' % (python_path, packages))
     if code == 0:
         print(out.decode())
     else:
-        print('exit with code',code)
+        print('exit with code', code)
         print(err.decode())
 
+
 def list_all():
-    out, err, code = run('conda env list --json')
+    out, err, code = run('%s env list --json' % CONDA_EXE)
 
     if code == 0:
         envs = json.loads(out.decode())
         for env in envs['envs']:
-            if env.find('envs')<0:
+            if env.find('envs') < 0:
                 name = 'base'
             else:
                 name = os.path.split(env)[-1]
             print(name)
 
-            out, err, code = run('conda list -n %s --json'%name)
+            out, err, code = run('%s list -n %s --json' % (CONDA_EXE, name))
             packages = json.loads(out.decode())
             for p in packages:
-                if p['name'].find('compas')==0:
-                    print('    %s:%s  %s'%(p['name'],p['version'],p['channel']))
+                if p['name'].find('compas') == 0:
+                    print('    %s:%s  %s' % (p['name'], p['version'], p['channel']))
             print('-'*20)
 
-
     else:
-        print('exit with code',code)
+        print('exit with code', code)
         print(err.decode())
+
 
 def RunCommand(is_interactive):
     list_all()
@@ -65,8 +73,7 @@ def RunCommand(is_interactive):
 # Main
 # ==============================================================================
 
+
 if __name__ == '__main__':
 
     RunCommand(True)
-
-
--- ui/Rhino/RV2/dev/COMPASinstall_cmd.py
+++ ui/Rhino/RV2/dev/COMPASinstall_cmd.py
@@ -6,11 +6,20 @@ import rhinoscriptsyntax as rs
 import subprocess
 import json
 import os
+import compas
+
+try:
+    from compas_bootstrapper import CONDA_EXE
+except ImportError:
+    CONDA_EXE = 'conda'
+
 
 def run(cmd):
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    stdout,stderr = p.communicate()
-    return stdout,stderr,p.returncode
+    # p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=compas._os.prepare_environment())
+    stdout, stderr = p.communicate()
+    return stdout, stderr, p.returncode
+
 
 def install_env_packages(env_name, envs=None, packages=None):
 
@@ -19,9 +28,9 @@ def install_env_packages(env_name, envs=None, packages=None):
         name = os.path.split(env)[-1]
         if name == env_name:
             env_path = env
-            python_path = os.path.join(env_path,'python.exe')
+            python_path = os.path.join(env_path, 'python.exe')
             if not os.path.isfile(python_path):
-                python_path = os.path.join(env_path,'bin','python')
+                python_path = os.path.join(env_path, 'bin', 'python')
 
             if packages:
                 to_install = packages
@@ -29,38 +38,39 @@ def install_env_packages(env_name, envs=None, packages=None):
                 packages = packages or list_package(env_name)
                 to_install = []
                 for p in packages:
-                    if p['name'].find('compas')==0:
-                        to_install.append(p['name'].replace('-','_'))
+                    if p['name'].find('compas') == 0:
+                        to_install.append(p['name'].replace('-', '_'))
 
             print(to_install)
 
             to_install = ' '.join(to_install)
-            cmd = '%s -m compas_rhino.install -p %s'%(python_path,to_install)
+            cmd = '%s -m compas_rhino.install -p %s' % (python_path, to_install)
             print(cmd)
             out, err, code = run(cmd)
             if code == 0:
                 print(out.decode())
                 print("Installation finished! Please restart rhino for changes to take effect!")
             else:
-                print('exit with code',code)
+                print('exit with code', code)
                 print(err.decode())
 
-            return 
+            return
 
     print('environment not found!')
 
 
 def list_package(name):
-    out, err, code = run('conda list -n %s --json'%name)
+    out, err, code = run('%s list -n %s --json' % (CONDA_EXE, name))
     packages = json.loads(out.decode())
     return packages
 
+
 def list_envs(show_packages=True):
-    out, err, code = run('conda env list --json')
+    out, err, code = run('%s env list --json' % CONDA_EXE)
     if code == 0:
         envs = json.loads(out.decode())
         for env in envs['envs']:
-            if env.find('envs')<0:
+            if env.find('envs') < 0:
                 name = 'base'
             else:
                 name = os.path.split(env)[-1]
@@ -68,25 +78,25 @@ def list_envs(show_packages=True):
             if show_packages:
                 packages = list_package(name)
                 for p in packages:
-                    if p['name'].find('compas')==0:
-                        print('    %s:%s  %s'%(p['name'],p['version'],p['channel']))
+                    if p['name'].find('compas') == 0:
+                        print('    %s:%s  %s' % (p['name'], p['version'], p['channel']))
             print('-'*20)
 
         return envs['envs']
 
     else:
-        print('exit with code',code)
+        print('exit with code', code)
         print(err.decode())
 
 
 def RunCommand(is_interactive):
 
     print('fetching conda envs....')
-    
+
     envs = list_envs(show_packages=False)
     envs_dict = {}
     for env_path in envs:
-        if env_path.find('envs')<0:
+        if env_path.find('envs') < 0:
             name = 'base'
         else:
             name = os.path.split(env_path)[-1]
@@ -95,20 +105,20 @@ def RunCommand(is_interactive):
 
     if options:
         selected = rs.ListBox(options, "Select env to install")
-        if selected: 
+        if selected:
             packages = list_package(selected)
             options = []
             for p in packages:
-                if p['name'].find('compas')==0:
-                    options.append((p['name'].replace('-','_'),False))
+                if p['name'].find('compas') == 0:
+                    options.append((p['name'].replace('-', '_'), False))
 
-            results = rs.CheckListBox(options, "Install compas packages to install from [%s]:"%selected)
+            results = rs.CheckListBox(options, "Install compas packages to install from [%s]:" % selected)
             if results:
                 to_install = []
-                for name,yes in results:
+                for name, yes in results:
                     if yes:
                         to_install.append(name)
-                install_env_packages(selected,envs=envs,packages=to_install)
+                install_env_packages(selected, envs=envs, packages=to_install)
 
 
 # ==============================================================================
