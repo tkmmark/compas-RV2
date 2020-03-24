869dd5fe3c6702f7b2a85986f22332262d41f452
Tom Van Mele <vanmelet@ethz.ch>
2020-03-24 10:20:50 +0100

    removing comments

--- src/compas_rv2/rhino/artists/meshartist.py
+++ src/compas_rv2/rhino/artists/meshartist.py
@@ -15,40 +15,6 @@ __all__ = ['MeshArtist']
 class MeshArtist(MeshArtist):
     """A customised `MeshArtist` for RV2 `Mesh`-based data structures."""
 
-    # def draw_vertexlabels(self, text, color):
-    #     if isinstance(text, dict):
-    #         keys = list(text.keys())
-    #     else:
-    #         keys = list(self.mesh.vertices())
-    #     guids = super(MeshArtist, self).draw_vertexlabels(text=text, color=color)
-    #     return list(zip(guids, keys))
-
-    # def draw_edgelabels(self, text, color):
-    #     if isinstance(text, dict):
-    #         keys = list(text.keys())
-    #     else:
-    #         keys = list(self.mesh.edges())
-    #     guids = super(MeshArtist, self).draw_edgelabels(text=text, color=color)
-    #     return list(zip(guids, keys))
-
-    # def draw_facelabels(self, text, color):
-    #     if isinstance(text, dict):
-    #         keys = list(text.keys())
-    #     else:
-    #         keys = list(self.mesh.faces())
-    #     guids = super(MeshArtist, self).draw_facelabels(text=text, color=color)
-    #     return list(zip(guids, keys))
-
-    # def draw_vertexnormals(self, color, scale):
-    #     keys = list(self.mesh.vertices())
-    #     guids = super(MeshArtist, self).draw_vertexnormals(keys=keys, color=color, scale=scale)
-    #     return list(zip(guids, keys))
-
-    # def draw_facenormals(self, color, scale):
-    #     keys = list(self.mesh.faces())
-    #     guids = super(MeshArtist, self).draw_facenormals(keys=keys, color=color, scale=scale)
-    #     return list(zip(guids, keys))
-
 
 # ==============================================================================
 # Main
