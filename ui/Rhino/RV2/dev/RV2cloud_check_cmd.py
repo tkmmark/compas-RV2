9c88ddf15ff750e8b78456d9a264826b50dc379a
Li <leochen1990@gmail.com>
2020-03-24 17:03:36 +0100

    add cloud control cmds

---
+++ ui/Rhino/RV2/dev/RV2cloud_check_cmd.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+from compas_rv2.rhino import get_proxy
+
+
+__commandname__ = "RV2cloud_check"
+
+
+def RunCommand(is_interactive):
+    p = get_proxy()
+    print(p.check())
+
+
+# ==============================================================================
+# Main
+# ==============================================================================
+
+
+if __name__ == "__main__":
+
+    RunCommand(True)
