38180b2ec4976a46f3284051c2e9486bbd68c8d3
Tom Van Mele <vanmelet@ethz.ch>
2020-03-26 12:39:30 +0100

    triangulation examples using compas_libigl

---
+++ examples/triangulations_libigl/triangulation2.py
@@ -0,0 +1,50 @@
+import os
+import json
+# import numpy
+import compas
+from compas.datastructures import Mesh
+from compas.geometry import centroid_points_xy
+from compas_plotters import MeshPlotter
+from compas.utilities import geometric_key
+from compas.utilities import pairwise
+from compas.rpc import Proxy
+# import compas_libigl as igl
+
+
+igl = Proxy('compas_libigl')
+
+
+HERE = os.path.dirname(__file__)
+DATA = os.path.join(HERE, '../../data')
+FILE = os.path.join(DATA, 'rhino1.json')
+
+with open(FILE, 'r') as f:
+    data = json.load(f)
+
+gkey_xyz = {geometric_key(point): point for point in data['boundary']}
+gkey_xyz.update({geometric_key(point): point for point in data['segments']})
+gkey_xyz.update({geometric_key(point): point for point in data['hole']})
+gkey_index = {gkey: index for index, gkey in enumerate(gkey_xyz.keys())}
+
+xyz = list(gkey_xyz.values())
+edges = []
+edges += [[gkey_index[geometric_key(a)], gkey_index[geometric_key(b)]] for a, b in pairwise(data['boundary'])]
+edges += [[gkey_index[geometric_key(a)], gkey_index[geometric_key(b)]] for a, b in pairwise(data['segments'])]
+edges += [[gkey_index[geometric_key(a)], gkey_index[geometric_key(b)]] for a, b in pairwise(data['hole'])]
+holes = []
+holes += [centroid_points_xy([xyz[gkey_index[geometric_key(point)]] for point in data['hole'][:-1]])]
+
+# V2, F2 = igl.delaunay_triangulation(V)
+# V2, F2 = igl.constrained_delaunay_triangulation(V, E)
+V2, F2 = igl.conforming_delaunay_triangulation(xyz, edges, holes, area=0.05)
+
+mesh = Mesh.from_vertices_and_faces(V2, F2)
+
+lines = []
+for u, v in edges:
+    lines.append({'start': xyz[u], 'end': xyz[v], 'color': '#ff0000', 'width': 2.0})
+
+plotter = MeshPlotter(mesh, figsize=(8, 5))
+plotter.draw_faces()
+plotter.draw_lines(lines)
+plotter.show()
