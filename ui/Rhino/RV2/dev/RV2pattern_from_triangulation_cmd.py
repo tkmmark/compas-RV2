1c2ab06ff41e2315188f3f19c2d6147062c0c811
Tom Van Mele <vanmelet@ethz.ch>
2020-03-27 17:22:15 +0100

    updated triangulation (untested)

--- ui/Rhino/RV2/dev/RV2pattern_from_triangulation_cmd.py
+++ ui/Rhino/RV2/dev/RV2pattern_from_triangulation_cmd.py
@@ -11,16 +11,12 @@ from compas.geometry import centroid_points_xy
 from compas.utilities import geometric_key
 from compas.utilities import pairwise
 import rhinoscriptsyntax as rs
-import math
 
 from compas.rpc import Proxy
+triangle = Proxy('compas_triangle.delaunay')
 
-igl = Proxy('compas_libigl')
 
 __commandname__ = "RV2pattern_from_triangulation"
-
-
-HERE = compas_rhino.get_document_dirname()
 
 
 def RunCommand(is_interactive):
@@ -29,65 +25,44 @@ def RunCommand(is_interactive):
     if not scene:
         return
 
-    boundary_guids = compas_rhino.select_curves('select outer boundary')
+    boundary_guids = compas_rhino.select_curves('Select outer boundary.')
     if not boundary_guids:
         return
 
-    hole_guids = compas_rhino.select_curves('select inner boundary')
-    segments_guids = compas_rhino.select_curves('select guide line')
-    target_length = rs.GetReal('target edge length')
+    hole_guids = compas_rhino.select_curves('Select inner boundaries.')
+    segments_guids = compas_rhino.select_curves('Select constraint curves.')
+
+    target_length = rs.GetReal('Specifiy target edge length.')
     if not target_length:
         return
 
     # boundary
-    boundary = RhinoCurve.from_guid(boundary_guids[0])
-    divide = math.floor(boundary.length()/target_length)
-    points = boundary.divide(divide, over_space=True)
-    boundary = points
+    curve = RhinoCurve.from_guid(boundary_guids[0])
+    N = int(curve.length() / target_length)
+    points = curve.divide(N, over_space=True)
+    boundary = map(list, points)
 
-    # hole
-    hole = []
-    if hole_guids:
-        hole = RhinoCurve.from_guid(hole_guids[0])
-        divide = math.floor(hole.length()/target_length)
-        points = hole.divide(divide, over_space=True)
-        hole = points
-
-    # segments
-    segments = []
+    # polylines
+    polylines = []
     if segments_guids:
-        segments = RhinoCurve.from_guid(segments_guids[0])
-        divide = math.floor(segments.length()/target_length)
-        points = segments.divide(divide, over_space=True)
-        segments = points
+        for guid in segments_guids:
+            curve = RhinoCurve.from_guid(guid)
+            N = int(curve.length() / target_length)
+            points = curve.divide(N, over_space=True)
+            polylines.append(map(list, points))
 
-    # data
-    data = {
-        "boundary": boundary,
-        "segments": segments,
-        "hole": hole,
-        "target_length": target_length
-    }
+    # polygons
+    polygons = []
+    if hole_guids:
+        for guid in hole_guids:
+            curve = RhinoCurve.from_guid(guid)
+            N = int(curve.length() / target_length)
+            points = curve.divide(N, over_space=True)
+            polygons.append(map(list, points))
 
-    gkey_xyz = {geometric_key(point): point for point in data['boundary']}
-    gkey_xyz.update({geometric_key(point): point for point in data['segments']})
-    gkey_xyz.update({geometric_key(point): point for point in data['hole']})
-    gkey_index = {gkey: index for index, gkey in enumerate(gkey_xyz.keys())}
-
-    xyz = list(gkey_xyz.values())
-    edges = []
-    edges += [[gkey_index[geometric_key(a)], gkey_index[geometric_key(b)]] for a, b in pairwise(data['boundary'])]
-    edges += [[gkey_index[geometric_key(a)], gkey_index[geometric_key(b)]] for a, b in pairwise(data['segments'])]
-    edges += [[gkey_index[geometric_key(a)], gkey_index[geometric_key(b)]] for a, b in pairwise(data['hole'])]
-
-    if data['hole']:
-        holes = []
-        holes += [centroid_points_xy([xyz[gkey_index[geometric_key(point)]] for point in data['hole'][:-1]])]
-    else:
-        holes = None
-
-    area = data['target_length'] ** 2 * 0.5 * 0.5 * 1.732
-    V2, F2 = igl.conforming_delaunay_triangulation(xyz, edges, holes, area=area)
+    area = target_length ** 2 * 0.5 * 0.5 * 1.732
+    vertices, faces = triangle.constrained_delaunay_triangulation(
+        boundary, polylines, polygons, area=area)
 
     pattern = Pattern.from_vertices_and_faces(V2, F2)
 
