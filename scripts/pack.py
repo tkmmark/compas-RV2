2b73f6b91f8ba78e3c8a73bfaac0a8f79fe63743
Li <leochen1990@gmail.com>
2020-06-09 12:06:29 +0200

    clean up unnecessary files before packaging

--- scripts/pack.py
+++ scripts/pack.py
@@ -7,8 +7,8 @@ import time
 import argparse
 
 parser = argparse.ArgumentParser(description='RhinoVault2 release package tool.')
-parser.add_argument('--skip_packing',action='store_true', help="skip packaging to dist folder")
-parser.add_argument('--rhi',action='store_true', help="pack into rhi installer")
+parser.add_argument('--skip_packing', action='store_true', help="skip packaging to dist folder")
+parser.add_argument('--rhi', action='store_true', help="pack into rhi installer")
 
 args = parser.parse_args()
 
@@ -19,9 +19,21 @@ conda_pack.pack(output="env.zip", verbose=True, n_threads=-1, force=True)
 print('unpacking to ui/Rhino/RV2/dev/env')
 shutil.unpack_archive("env.zip", "ui/Rhino/RV2/dev/env")
 
+print('removing unnecessary files')
+for root, dirs, files in os.walk("ui/Rhino/RV2/dev/env"):
+
+    for d in dirs:
+        if d.find("node_modules") >= 0:
+            shutil.rmtree(os.path.join(root, d))
+
+    for f in files:
+        if f.find("electron.zip") >= 0:
+            os.remove(os.path.join(root, f))
+
+
 if args.skip_packing:
 
-    print('finished, took %s s'%(time.time()-start))
+    print('finished, took %s s' % (time.time()-start))
     print('packing skipped, go to ui/Rhino/RV2/dev and run install.bat(win) or install.command(mac)')
 
 else:
@@ -34,11 +46,11 @@ else:
         os.mkdir("dist")
 
     if args.rhi:
-        shutil.make_archive( "dist/RV2", "zip", "ui/Rhino/RV2/dev")
-        os.rename("dist/RV2.zip","dist/RV2.rhi")
+        shutil.make_archive("dist/RV2", "zip", "ui/Rhino/RV2/dev")
+        os.rename("dist/RV2.zip", "dist/RV2.rhi")
     else:
-        shutil.make_archive( "dist/RV2", "zip", "ui/Rhino/RV2")
+        shutil.make_archive("dist/RV2", "zip", "ui/Rhino/RV2")
 
-    shutil.rmtree("ui/Rhino/RV2/dev/env")    
+    shutil.rmtree("ui/Rhino/RV2/dev/env")
 
-    print('finished, took %s s'%(time.time()-start)) 
+    print('finished, took %s s' % (time.time()-start))
