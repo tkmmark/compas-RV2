b9cd0c62f1423ed6e87b27b2cbfb583bf615527a
Li <leochen1990@gmail.com>
2020-06-09 12:07:02 +0200

    add release batch script

---
+++ scripts/release.bat
@@ -0,0 +1,10 @@
+conda env remove -n rv2
+conda create -n rv2 python=3.7 conda-pack cython -y
+conda activate rv2
+pip install compas==0.15.6
+pip install git+https://github.com/BlockResearchGroup/compas_cloud.git#egg=compas_cloud
+pip install git+https://github.com/BlockResearchGroup/compas_tna.git#egg=compas_tna
+pip install git+https://github.com/BlockResearchGroup/compas_skeleton.git#egg=compas_skeleton
+pip install git+https://github.com/BlockResearchGroup/compas_triangle.git#egg=compas_triangle
+pip install .
+python scripts/pack.py
