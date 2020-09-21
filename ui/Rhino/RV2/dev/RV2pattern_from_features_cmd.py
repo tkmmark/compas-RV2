f49b46ce18b2a37d434c928117e72cb31bd1b73c
Tom Van Mele <vanmelet@ethz.ch>
2020-09-22 00:27:05 +0200

    update singular

--- ui/Rhino/RV2/dev/RV2pattern_from_features_cmd.py
+++ ui/Rhino/RV2/dev/RV2pattern_from_features_cmd.py
@@ -2,22 +2,89 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
+from compas.utilities import geometric_key
+from compas.utilities import linspace
+
 import compas_rhino
+from compas_rhino.artists import MeshArtist
+from compas_rhino.geometry import RhinoPoint
+
+from compas_singular.algorithms import SkeletonDecomposition
+from compas_singular.algorithms import boundary_triangulation
+from compas_singular.rhino import RhinoSurface
+from compas_singular.rhino import RhinoCurve
+from compas_singular.rhino import automated_smoothing_surface_constraints
+from compas_singular.rhino import automated_smoothing_constraints
+from compas_singular.rhino import constrained_smoothing
+
 from compas_rv2.datastructures import Pattern
 from compas_rv2.rhino import get_scene
 from compas_rv2.rhino import get_proxy
-
-from compas_singular.datastructures.mesh.constraints import automated_smoothing_surface_constraints, automated_smoothing_constraints
-from compas_singular.datastructures.mesh.relaxation import constrained_smoothing
 from compas_rv2.rhino import rv2_undo
 
 
 __commandname__ = "RV2pattern_from_features"
 
 
+def pattern_from_features(srf_guid, crv_quids, pt_guids, L):
+    # Wrap the inputs.
+    surface = RhinoSurface.from_guid(srf_guid)
+    curves = [RhinoCurve.from_guid(guid) for guid in crv_guids]
+    points = [RhinoPoint.from_guid(guid) for guid in pt_guids]
+
+    # Compute the feature discretisation length.
+    box = compas_rhino.rs.BoundingBox([srf_guid])
+    diagonal = compas_rhino.rs.Distance(box[0], box[6])
+    D = 0.01 * diagonal
+
+    # Process the input surface.
+    result = surface.discrete_mapping(D, crv_guids=crv_guids, pt_guids=pt_guids)
+    outer_boundary, inner_boundaries, polyline_features, point_features = result
+
+    # Triangulate the input surface.
+    trimesh = boundary_triangulation(*result, delaunay=delaunay)
+
+    # Make a decomposition mesh from the triangulation.
+    decomposition = SkeletonDecomposition.from_mesh(trimesh)
+
+    # Generate a coarse mesh from the decomposition.
+    coarsemesh = decomposition.decomposition_mesh(point_features)
+
+    # Map coarse mesh edges to surface curve discretisations.
+    gkey_vertex = {geometric_key(coarsemesh.vertex_coordinates(vertex)): vertex for vertex in coarsemesh.vertices()}
+    edge_curve = {}
+    for polyline in decomposition.polylines:
+        curve = compas_rhino.rs.AddInterpCrvOnSrfUV(srf_guid, [point[:2] for point in polyline])
+        u = gkey_vertex[geometric_key(polyline[0])]
+        v = gkey_vertex[geometric_key(polyline[-1])]
+        edge_curve[u, v] = [
+            compas_rhino.rs.EvaluateCurve(curve, compas_rhino.rs.CurveParameter(curve, t))
+            for t in linspace(0, 1, 100)]
+        compas_rhino.delete_object(curve)
+
+    # Densify the coarse mesh.
+    coarsemesh.collect_strips()
+    coarsemesh.set_strips_density_target(L)
+    coarsemesh.densification(edges_to_curves=edge_curve)
+    densemesh = coarsemesh.get_quad_mesh()
+
+    # Create pattern.
+    pattern = Pattern.from_vertices_and_faces(*densemesh.to_vertices_and_faces())
+
+    # Constrain mesh components to the feature geometry.
+    constraints = automated_smoothing_surface_constraints(pattern, surface)
+    constraints.update(
+        automated_smoothing_constraints(pattern, rhinopoints=points, rhinocurves=curves))
+
+    # Smooth with constraints.
+    constrained_smoothing(
+        pattern, kmax=10, damping=0.5, constraints=constraints, algorithm="area")
+
+    return pattern
+
+
 @rv2_undo
 def RunCommand(is_interactive):
-
     scene = get_scene()
     if not scene:
         return
@@ -26,51 +93,25 @@ def RunCommand(is_interactive):
     if not proxy:
         return
 
-    srf_guid = compas_rhino.select_surface("Select a surface.")
+    # Get input data.
+    srf_guid = compas_rhino.select_surface("Select a surface to decompose.")
     if not srf_guid:
         return
 
     crv_guids = []
-    pt_guids = compas_rhino.select_points("Step 1/3 (Optional) - Select points for pole singularities.") or []
+    # crv_guids = compas_rhino.select_curves("Select curves to include in the decomposition.")
+    pt_guids = compas_rhino.select_points("Select points to include in the decomposition.")
 
-    box = compas_rhino.rs.BoundingBox([srf_guid])
-    input_subdivision_spacing = 0.01 * compas_rhino.rs.Distance(box[0], box[6])
+    # Get the target length for the final quad mesh.
+    L = compas_rhino.rs.GetReal("Define the target edge length of the pattern.", 1.0)
 
-    mesh_edge_length = compas_rhino.rs.GetReal("Step 2/3 - Enter target length for edges.", 1.0)
-
-    delaunay = proxy.function("compas.geometry.triangulation.triangulation_numpy.delaunay_from_points_numpy")
-
-    pattern = Pattern.from_surface_and_features(input_subdivision_spacing, mesh_edge_length, srf_guid, crv_guids, pt_guids, delaunay)
+    # Generate the pattern
+    pattern = pattern_from_features(srf_guid, crv_guids, pt_guids, L)
+    if not pattern:
+        return
 
     scene.clear()
     scene.add(pattern, name='pattern')
-    scene.update()
-
-    kmax = 10
-
-    while True:
-        option = compas_rhino.rs.GetString("Step 3/3 (Optional) - Press Enter to run constrained Laplacian smoothing or ESC to skip.", strings=['Iterations'])
-
-        if option is None:
-            break
-
-        if not option:
-            constraints = automated_smoothing_surface_constraints(pattern, srf_guid)
-            constraints.update(automated_smoothing_constraints(pattern, points=pt_guids, curves=crv_guids))
-            constrained_smoothing(pattern, kmax=kmax, damping=0.5, constraints=constraints, algorithm='area')
-            objs = set(constraints.values())
-            inputs = [srf_guid] + crv_guids + pt_guids
-            for obj in objs:
-                if obj not in inputs:
-                    compas_rhino.rs.DeleteObject(obj)
-            compas_rhino.rs.HideObjects(inputs)
-            break
-
-        if option == 'Iterations':
-            new_kmax = compas_rhino.rs.GetInteger("Number of iterations:", kmax)
-            if new_kmax or new_kmax is not None:
-                kmax = new_kmax
-
     scene.update()
 
     print('Pattern object successfully created. Input object has been hidden.')
