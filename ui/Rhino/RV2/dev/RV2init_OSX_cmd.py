bc475279cc6041813727d9a187cb5e953183ed10
Tom Van Mele <vanmelet@ethz.ch>
2020-09-22 11:43:17 +0200

    adding an OSX init for debugging on Mac

---
+++ ui/Rhino/RV2/dev/RV2init_OSX_cmd.py
@@ -0,0 +1,67 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+import os
+import scriptcontext as sc
+
+import compas_rhino
+
+from compas_cloud import Proxy  # noqa: E402
+from compas_rv2.scene import Scene  # noqa: E402
+from compas_rv2.rhino import ErrorHandler  # noqa: E402
+
+
+__commandname__ = "RV2init_OSX"
+
+
+SETTINGS = {
+
+    "RV2": {
+        "show.forces": False,
+        "show.angles": True,
+        "tol.angles": 5.0
+    },
+
+    "Solvers": {
+        "tna.vertical.kmax": 300,
+        "tna.vertical.zmax": 4.0,
+        "tna.horizontal.kmax": 100,
+        "tna.horizontal.alpha": 100,
+        "tna.horizontal.refreshrate": 10,
+    }
+
+}
+
+
+HERE = compas_rhino.get_document_dirname()
+HOME = os.path.expanduser('~')
+CWD = HERE or HOME
+
+
+def RunCommand(is_interactive):
+
+    errorHandler = ErrorHandler(title="Server side Error", showLocalTraceback=False)
+    sc.sticky["RV2.proxy"] = Proxy(errorHandler=errorHandler)
+
+    sc.sticky["RV2.system"] = {
+        "session.dirname": CWD,
+        "session.filename": None,
+        "session.extension": 'rv2'
+    }
+
+    scene = Scene(SETTINGS)
+    scene.clear()
+
+    sc.sticky["RV2"] = {"scene": scene}
+
+    sc.sticky["RV2.sessions"] = []
+
+
+# ==============================================================================
+# Main
+# ==============================================================================
+
+if __name__ == '__main__':
+
+    RunCommand(True)
