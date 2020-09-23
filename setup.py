fb42a9ce2d1708b6c667a78b2eb822b2ae270759
Li <leochen1990@gmail.com>
2020-09-23 17:09:05 +0200

    update dependencies

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ optional_requirements = {}
 
 setup(
     name='compas_rv2',
-    version='0.1.0',
+    version='1.1.0',
     description='RhinoVault for Rhino 6 based on COMPAS',
     long_description=long_description,
     long_description_content_type='text/markdown',
